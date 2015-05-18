a2bb1379a227b6805e9a7ec8dd2c0ff025f9e61b
Tony Kay <twkay@thenavisway.com>
2015-05-18 09:31:38 -0700

    added haskell examples

---
+++ haskell/fib.hs
@@ -0,0 +1,8 @@
+fib = 0:1:zipWith (+) fib (tail fib)
+
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
