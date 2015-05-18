50eee74374ed3799ed1c9c98704a05318a7f5611
Tony Kay <twkay@thenavisway.com>
2015-05-18 11:42:47 -0700

    added more code to support talk

---
+++ multimethod.clj
@@ -0,0 +1,27 @@
+(ns example)
+
+;; Define "area" functionality to dispatch based on the :Shape key (implies "area" accepts a map as an argument)
+(defmulti area :Shape)
+
+;; Define a function that makes a map where key :Shape -> :Rect
+(defn rect [wd ht] {:Shape :Rect :wd wd :ht ht})
+
+;; Define a function that makes a map where key :Shape -> :Circle
+(defn circle [radius] {:Shape :Circle :radius radius})
+
+;; Extend the "area" functionality to support :Rect
+(defmethod area :Rect [r]
+    (* (:wd r) (:ht r)))
+
+;; Extend the "area" functionality to support :Circle
+(defmethod area :Circle [c]
+    (* (. Math PI) (* (:radius c) (:radius c))))
+
+(def r1 (rect 10 20))
+(def c1 (circle 100))
+
+(area r1)
+(area c1)
+
+; Some other (futute) file wants to extend supported shapes to include Squares:
+; (area {:Shape :Square :side 10})
--- sort2.hs
+++ sort2.hs
@@ -2,9 +2,12 @@ data TShirtSize = XSmall | Small | Medium | Large | XLarge | XXLarge deriving (S
 data Color = Red|Green|Blue|Yellow|Orange deriving (Eq, Show)
 data TShirt = TShirt TShirtSize Color deriving (Eq, Show)
 
+-- Ord is defined in library. No need to recompile it. 
+-- Adding "shirt" support for the "Ord" typeclass is trivial, and NOT tied to inheritance
 instance Ord TShirt where
    compare (TShirt sz1 color) (TShirt sz2 color2) = compare sz1 sz2
 
+-- Could have been defined in a library ages ago...has no knowledge of shirts at all
 quicksort :: (Ord a) => [a] -> [a]  
 quicksort [] = []  
 quicksort (x:xs) =   
