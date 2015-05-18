9f201f8653d624efb7819915102b2775c11e4f42
Tony Kay <twkay@thenavisway.com>
2015-05-18 12:24:07 -0700

    added a bit more to the scala code, for clarity

--- scala/src/main/scala/example/Schema.scala
+++ scala/src/main/scala/example/Schema.scala
@@ -2,7 +2,10 @@ package example
 
 import org.squeryl.{Schema, KeyedEntity}
 
-case class Person(val id : Long, val name : String, val age: Int, val likeCarrots : Boolean) extends KeyedEntity[Long]
+case class Person(id: Long,
+                  name: String,
+                  age: Int,
+                  likeCarrots: Boolean) extends KeyedEntity[Long]
 
 object MySchema extends Schema {
   val people = table[Person]
--- scala/src/test/scala/example/SampleSpec.scala
+++ scala/src/test/scala/example/SampleSpec.scala
@@ -3,17 +3,51 @@ package example
 import java.io.File
 
 import example.MySchema.people
-import org.scalatest.{BeforeAndAfterEach, FlatSpec, MustMatchers}
+import org.scalatest.{BeforeAndAfterAll, FlatSpec, MustMatchers}
 import org.squeryl.PrimitiveTypeMode._
 import org.squeryl.adapters.H2Adapter
 import org.squeryl.{Session, SessionFactory}
 
-class SampleSpec extends FlatSpec with BeforeAndAfterEach with MustMatchers {
+class SampleSpec extends FlatSpec with BeforeAndAfterAll with MustMatchers {
+
+  override def beforeAll(): Unit = {
+    Setup()
+  }
+
+  behavior of "Queries"
+
+  it should "be able to query with a where clause" in {
+    Console.println("A********************************************************************************")
+    transaction {
+      from(people)(p =>
+        where(p.name === "tony")
+          select p).headOption
+    }
+  }
+
+  it should "honor Option context to include/exclude optional predicates" in {
+    Console.println("B********************************************************************************")
+    val optionalName: Option[String] = Some("Liz")
+    val optionalAge: Option[Int] = Some(42)
+    val optionalLikesCarrots: Option[Boolean] = None
+
+    transaction {
+      from(people)(p =>
+        where(
+          p.name === optionalName.? and
+            p.likeCarrots === optionalLikesCarrots.? and
+            p.age > optionalAge.?)
+          select p).headOption
+    }
+  }
+}
+
+object Setup {
   Class.forName("org.h2.Driver")
 
-  override def beforeEach() : Unit = {
+  def apply() = {
     val directory = new File("/tmp")
-    for(f <- directory.listFiles() if f.getName.startsWith("test.db"))
+    for (f <- directory.listFiles() if f.getName.startsWith("test.db"))
       f.delete()
     SessionFactory.concreteFactory = Some(() => {
       val session = Session.create(
@@ -24,40 +58,6 @@ class SampleSpec extends FlatSpec with BeforeAndAfterEach with MustMatchers {
     })
     transaction {
       example.MySchema.create
-    }
-  }
-
-  behavior of "Samples"
-
-  it should "do things" in {
-    transaction {
-      from(people)(p =>
-        where(p.name === "tony")
-          select p).headOption
-    }
-  }
-
-  behavior of "Other things"
-
-  it should "include optional stuff that is present" in {
-    val optionalName : Option[String] = Some("Tony")
-    transaction {
-      from(people)(p =>
-        where(p.name === optionalName.?)
-          select p).headOption
-    }
-  }
-
-  it should "not include optional stuff that is not present" in {
-    val optionalName : Option[String] = Some("Liz")
-    val optionalAge : Option[Int] = Some(42)
-
-    transaction {
-      from(people)(p =>
-        where(
-          p.name === optionalName.? and
-          p.age > optionalAge.?)
-          select p).headOption
     }
   }
 }
