73d2a93efac198136ea579182716383ef9ddc3ad
Tony Kay <twkay@thenavisway.com>
2015-05-19 08:37:26 -0700

    improved scala expression problem sample

--- scala/src/main/scala/example/Shape1.scala
+++ scala/src/main/scala/example/Shape1.scala
@@ -1,28 +1,48 @@
 package example
 
-case class Rect(w : Double, h : Double)
-case class Circle(r : Double)
-
+// like a namespace
 object Geometry {
-  // PROBLEM: Adding support for new shape requires access to THIS source file
-  def area(r : Rect) = r.w * r.h
-  def area(c : Circle) = Math.PI * c.r * c.r
-}
+  // Attempt 1 ********************************************************************************
+  // Can define a general purpose function in one place...
+  trait Shape
+  case class Rect(w: Double, h: Double) extends Shape
+  case class Circle(r: Double) extends Shape
 
-// Imagine some other source:
-// Geometry.area(someTriangle); // error!
+  def area(s: Shape) = s match {
+    case Rect(w, h) => w * h
+    case Circle(r) => Math.PI * r * r
+    // But extending it requires changing the source of this function
+    case _ => throw new IllegalArgumentException("Unsupported Shape")
+  }
+
+  // Attempt 2 ********************************************************************************
+  // I could try making a set of "global functions"
+  def area2(r: Rect) = r.w * r.h
+  def area2(c: Circle) = Math.PI * c.r * c.r
+
+  // New shape with area support: Just write new code. Excellent!
+  case class Square(s: Double) extends Rect(s, s)
+  def area2(s: Square) = s.s * s.s
+
+  // But if I want to use polymorphism...
+  var someShape: Shape = Circle(3.5)
+
+  // I'm screwed...compiler cannot resolve
+  area2(someShape)
+}
 
 // OK, how about inheritance???
-trait Shape {
-  def area : Double
+trait Shape2 {
+  def area: Double
 }
 
-case class SRect(w : Double, h : Double) extends Shape {
+case class SRect(w: Double, h: Double) extends Shape2 {
   def area = w * h
 }
 
-case class SCircle(r : Double) extends Shape {
+case class SCircle(r: Double) extends Shape2 {
   def area = Math.PI * r * r
 }
 
-// All great and wonderful, but now define "perimiter"! Ooops...need to change Shape!
+// All great and wonderful, but now define "perimiter"! Ooops...need to change all of the original
+// code! I cannot add new functions over the existing types.
